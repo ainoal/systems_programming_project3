1fbd289b85f055f73523ef7a7a84665b75b994de
ainoal <aino.liukkonen@gmail.com>
2021-04-23 22:15:48 +0300

    started project 3

---
+++ wish.c
@@ -0,0 +1,30 @@
+/*******************************************************************/
+/* CT30A3370 Käyttöjärjestelmät ja systeemiohjelmointi
+ * wish.c
+ * Aino Liukkonen
+ * 23.4.2021
+ */
+/*******************************************************************/
+
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+	
+	switch(argc) {
+		case 1:
+			// interactive mode
+			break;
+		case 2:
+			// batch mode
+			break;
+		default:
+			// What to do if there's too many args?
+			break;
+	}
+
+	return 0;
+}
+
+
+/*******************************************************************/
+/* eof */
