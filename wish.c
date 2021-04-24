3a4f66cb26d1efaada05969283ca3547c8ed4e7f
ainoal <aino.liukkonen@gmail.com>
2021-04-24 18:23:16 +0300

    interactive wish loop with calloc, not running any args yet

--- wish.c
+++ wish.c
@@ -2,24 +2,37 @@
 /* CT30A3370 Käyttöjärjestelmät ja systeemiohjelmointi
  * wish.c
  * Aino Liukkonen
- * 23.4.2021
+ * 24.4.2021
  */
 /*******************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include "wish.h"
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
+	char **arg = NULL;	
+	char *buffer = NULL;
+	size_t bufferSize = 0;
+	ssize_t lineLen;
 	
-	switch(argc) {
+	switch (argc) {
 		case 1:
-			// interactive mode
-			break;
+			while (1) {
+				printf("wish> ");
+
+				if ((lineLen = getline(&buffer, &bufferSize, stdin)) != -1) {
+					if ((arg = calloc(MAX_ARGS, sizeof(__intptr_t))) == NULL) {
+						perror("Calloc error\n"); // ???
+					}
+				} else { break;}
+			}
 		case 2:
 			// batch mode
 			break;
-		default:
-			// What to do if there's too many args?
-			break;
+		default: 	// If there are more than 1 command line arguments
+			printf("Usage: ./wish [batch-file]\n"); 
+			exit(1);
 	}
 
 	return 0;
---
+++ wish.h
@@ -0,0 +1,15 @@
+/*******************************************************************/
+/* CT30A3370 Käyttöjärjestelmät ja systeemiohjelmointi
+ * wish.c
+ * Aino Liukkonen
+ * 24.4.2021
+ */
+/*******************************************************************/
+
+/* MAX_ARGS defines the maximum number of arguments that user can give
+when running commands. It is needed for successful memory allocation 
+with calloc() */
+#define MAX_ARGS 30
+
+/*******************************************************************/
+/* eof */
